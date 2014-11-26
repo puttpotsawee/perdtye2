7d35499f9cc4eea60efd3a872b75234071ce16ac
puttpotsawee <junrai82@gmail.com>
2014-11-26 22:04:56 +0700

    Revert "Pae"
    This reverts commit b990d11a0ff753f09783e8edc6761a7aef11704f.

--- app/views/perdtye/auctiontype.blade.php
+++ app/views/perdtye/auctiontype.blade.php
@@ -34,15 +34,15 @@
 
 										</div>
 
-										<!-- Controls -->
-										<a class="carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+										<!-- Controls --><!-- 
+										<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
 											<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 											<span class="sr-only">Previous</span>
-										</a><!-- 
+										</a> -->
 										<a class="carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
 											<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 											<span class="sr-only">Next</span>
-										</a> -->
+										</a>
 
 									</div>
 								</div>
@@ -173,7 +173,7 @@
 			</div>
 		</div>
 	</div>
-	<!-- auction
+	<!-- auction -->
 	@stop
 
 	@section('footer')
