6151f1109c2a7a61571b65bbb53889a91cb00315
puttpotsawee <junrai82@gmail.com>
2014-11-26 22:06:31 +0700

    Revert "pic"
    This reverts commit b002fb20cf876856c26a936153d45434399634b9.
    
    Conflicts:
    	app/views/perdtye/auctiontype.blade.php
    	app/views/perdtye/directtype.blade.php

--- app/views/perdtye/auctiontype.blade.php
+++ app/views/perdtye/auctiontype.blade.php
@@ -4,6 +4,7 @@
 		$product
 		$details
 		$seller
+<<<<<<< HEAD
 	-->
 
 	@section('content')
@@ -46,31 +47,41 @@
 
 									</div>
 								</div>
+=======
+-->
+
+@section('content')
+<!-- auction -->
+<div class="container">
+	<div class="row clearfix" style="margin-top:30px;">
+		<div class="col-md-12 column">
+			<div class="row clearfix">
+				<div class="col-md-2 column">
+				</div>
+				<div class="col-md-8 column">
+					<legend> {{$product->product_name}} </legend>
+					<div class="row clearfix" style="margin-top:30px;">
+						<div class="col-md-6 column">
+							<div class="thumbnail">
+								<img src="img/i6.png" width="100%"/>
+>>>>>>> parent of b002fb2... pic
 							</div>
-							<div class="col-md-6 column">
-								<form action="placebid" method="post">
-									<div class="row">
-										<div class="col-md-5" >
-											<h4>End time :</h4>
-										</div>
-										<div class="col-md-7" >
-											<h4> {{$details->end_time}} </h4>
-										</div>
+						</div>
+						<div class="col-md-6 column">
+							<form action="placebid" method="post">
+								<div class="row">
+									<div class="col-md-5" >
+										<h4>End time :</h4>
 									</div>
-									<div class="row" style="margin-top:0px;">
-										<div class="col-md-5">
-											<h3>Current bid</h3>
-										</div>
-										<div class="col-md-7" >
-											<h3>{{$details->current_price}} Baht</h3>
-										</div>  
+									<div class="col-md-7" >
+										<h4> {{$details->end_time}} </h4>
 									</div>
-									<div class="row" style="margin-top:20px;">
-										<div class="col-md-12">
-											<button type="submit" class="btn btn-info"style="width:100%">Place bid at {{$details->bidding_range+$details->current_price}} Baht </button>
-											<input name="idProduct" type="hidden" value={{$product->idProduct}}/>
-										</div>
+								</div>
+								<div class="row" style="margin-top:0px;">
+									<div class="col-md-5">
+										<h3>Current bid</h3>
 									</div>
+<<<<<<< HEAD
 
 
 								</form>
@@ -85,97 +96,123 @@
 										<div class="col-md-6" >
 											<button type="submit" class="btn btn-primary"style="width:100%">Place bid</button>
 										</div>  
-									</div>
-
-								</form>
-
-								<div class="row" style="margin-top:40px;">
-									<div class="col-md-6" >
-										<p>Sell information : {{$seller->name}} {{$seller->surname}}</p>
-										<p>100% positive feedback</p>
-									</div>
-									<div class="col-md-6" >
-										<h3 style="color:#3399FF;">&#9733; &#9733; &#9733; &#9733; &#9734;</h3>
-									</div>
+=======
+									<div class="col-md-7" >
+										<h3>{{$details->current_price}} Baht</h3>
+									</div>  
 								</div>
 								<div class="row" style="margin-top:20px;">
 									<div class="col-md-12">
-										<a href="qa" class="btn btn-default"style="width:100%">Ask a question </a>
+										<button type="submit" class="btn btn-info"style="width:100%">Place bid at {{$details->bidding_range+$details->current_price}} Baht </button>
+										<input name="idProduct" type="hidden" value={{$product->idProduct}}/>
+>>>>>>> parent of b002fb2... pic
 									</div>
+								</div>
+								
+
+							</form>
+
+							<form action="maxbid" method="post">
+								<div class="row" style="margin-top:20px;">
+									<div class="col-md-6">
+										<input name="maxbid_input" type="text" data-validation="number" class="form-control" style="width:100%;"></input>
+										<input name="idProduct" type="hidden" value={{$product->idProduct}}/>
+										<p><center>specify your maximum bid</center></p>
+									</div>
+									<div class="col-md-6" >
+										<button type="submit" class="btn btn-primary"style="width:100%">Place bid</button>
+									</div>  
+								</div>
+
+							</form>
+
+							<div class="row" style="margin-top:40px;">
+								<div class="col-md-6" >
+									<p>Sell information : {{$seller->name}} {{$seller->surname}}</p>
+									<p>100% positive feedback</p>
+								</div>
+								<div class="col-md-6" >
+									<h3 style="color:#3399FF;">&#9733; &#9733; &#9733; &#9733; &#9734;</h3>
 								</div>
 							</div>
-						</div>
-						<div class="col-md-12 column">
-							<div class="well-shadow">
-								<legend>Description</legend>
-								<div class="row" style="margin-top:20px;">
-									<div class="col-md-3 column">
-										<p>Name :</p>
-									</div>
-									<div class="col-md-9 column">
-										<p> {{$product->product_name}} </p>
-									</div>
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-12">
+									<a href="qa" class="btn btn-default"style="width:100%">Ask a question </a>
 								</div>
-
-								<div class="row" style="margin-top:20px;">
-									<div class="col-md-3 column">
-										<p>Product Information :</p>
-									</div>
-									<div class="col-md-9 column">
-										<p style="word-wrap: break-word;">{{$product->brand}}</p>
-										<p style="word-wrap: break-word;">{{$product->model}}</p>
-										<p style="word-wrap: break-word;">{{$product->property}}</p>
-										<p style="word-wrap: break-word;">{{$product->condition}}</p>
-										<p style="word-wrap: break-word;">{{$product->defect}}</p>
-									</div>
-								</div>
-
-								<div class="row" style="margin-top:20px;">
-									<div class="col-md-3 column">
-										<p>Technical specification :</p>
-									</div>
-									<div class="col-md-9 column">
-										<p style="word-wrap: break-word;">{{$product->dimension}}</p>
-									</div>
-								</div>
-
-								<div class="row" style="margin-top:20px;">
-									<div class="col-md-3 column">
-										<p>Shipping and payments :</p>
-									</div>
-									<div class="col-md-9 column">
-										<p style="word-wrap: break-word;">{{$product->payment_method}}</p>
-										<p style="word-wrap: break-word;">{{$product->packaging_method}}</p>
-										<p style="word-wrap: break-word;">{{$product->packaging_charge}}</p>
-										<p style="word-wrap: break-word;">{{$product->shipping_method}}</p>
-										<p style="word-wrap: break-word;">{{$product->shipping_fee}}</p>
-									</div>
-								</div>
-
-								<div class="row" style="margin-top:20px;">
-									<div class="col-md-3 column">
-										<p>Return policy :</p>
-									</div>
-									<div class="col-md-9 column">
-										<p style="word-wrap: break-word;">{{$product->refund_policy}}</p>
-										<p style="word-wrap: break-word;">{{$product->refund_charge}}</p>
-									</div>
-								</div>
-
-
 							</div>
 						</div>
 					</div>
-					<div class="col-md-2 column">
+					<div class="col-md-12 column">
+						<div class="well-shadow">
+							<legend>Description</legend>
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-3 column">
+									<p>Name :</p>
+								</div>
+								<div class="col-md-9 column">
+									<p> {{$product->product_name}} </p>
+								</div>
+							</div>
+
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-3 column">
+									<p>Product Information :</p>
+								</div>
+								<div class="col-md-9 column">
+									<p style="word-wrap: break-word;">{{$product->brand}}</p>
+									<p style="word-wrap: break-word;">{{$product->model}}</p>
+									<p style="word-wrap: break-word;">{{$product->property}}</p>
+									<p style="word-wrap: break-word;">{{$product->condition}}</p>
+									<p style="word-wrap: break-word;">{{$product->defect}}</p>
+								</div>
+							</div>
+
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-3 column">
+									<p>Technical specification :</p>
+								</div>
+								<div class="col-md-9 column">
+									<p style="word-wrap: break-word;">{{$product->dimension}}</p>
+								</div>
+							</div>
+
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-3 column">
+									<p>Shipping and payments :</p>
+								</div>
+								<div class="col-md-9 column">
+									<p style="word-wrap: break-word;">{{$product->payment_method}}</p>
+									<p style="word-wrap: break-word;">{{$product->packaging_method}}</p>
+									<p style="word-wrap: break-word;">{{$product->packaging_charge}}</p>
+									<p style="word-wrap: break-word;">{{$product->shipping_method}}</p>
+									<p style="word-wrap: break-word;">{{$product->shipping_fee}}</p>
+								</div>
+							</div>
+
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-3 column">
+									<p>Return policy :</p>
+								</div>
+								<div class="col-md-9 column">
+									<p style="word-wrap: break-word;">{{$product->refund_policy}}</p>
+									<p style="word-wrap: break-word;">{{$product->refund_charge}}</p>
+								</div>
+							</div>
+
+
+						</div>
 					</div>
 				</div>
-
+				<div class="col-md-2 column">
+				</div>
 			</div>
+
 		</div>
 	</div>
-	<!-- auction -->
-	@stop
+</div>
+<!-- auction -->
+@stop
 
-	@section('footer')
+@section('footer')
 	<nav class="navbar navbar-default navbar-bottom2" role="navigation">
-		@stop
+@stop
--- app/views/perdtye/directtype.blade.php
+++ app/views/perdtye/directtype.blade.php
@@ -4,8 +4,9 @@
 		$product
 		$details
 		$seller
-	-->
+-->
 
+<<<<<<< HEAD
 	@section('content')
 	<!-- direct -->
 	<div class="container" style="margin-top:30px;">
@@ -47,120 +48,137 @@
 
 									</div>
 								</div>
+=======
+@section('content')
+<!-- direct -->
+<div class="container" style="margin-top:30px;">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<div class="row clearfix">
+				<div class="col-md-2 column">
+				</div>
+				<div class="col-md-8 column">
+					<legend> {{$product->product_name}} </legend>
+					<div class="row clearfix" style="margin-top:30px;">
+						<div class="col-md-6 column">
+							<div class="thumbnail">
+								<img src="img/i6.png" width="100%"/>
+>>>>>>> parent of b002fb2... pic
 							</div>
-							<div class="col-md-6 column">
-								<form action=direct method="post">
-									<div class="row">
-										<div class="col-md-5" >
-											<h3>Price :</h3>
-										</div>
-										<div class="col-md-7" >
-											<h3> {{$details->price}} Baht</h3>
-										</div>
+						</div>
+						<div class="col-md-6 column">
+							<form action=direct method="post">
+								<div class="row">
+									<div class="col-md-5" >
+										<h3>Price :</h3>
 									</div>
-									<div class="row" style="margin-top:20px;">
-										<div class="col-md-5">
-											<h4>Quantity: </h4>
-											<label>({{$details->remaining}} in stock)</label>
-										</div>
-										<div class="col-md-7" >
-											<input name="quantity_input" type="text" data-validation="number" class="form-control" style="width:100%; float:right;"></input>
-											<input name="idProduct" type="hidden" value={{$product->idProduct}}/>
-										</div>  
+									<div class="col-md-7" >
+										<h3> {{$details->price}} Baht</h3>
 									</div>
-									<div class="row" style="margin-top:20px;">
-										<div class="col-md-12">
-											<button type="submit" class="btn btn-info"style="width:100%">Buy now </button>
-										</div>
+								</div>
+								<div class="row" style="margin-top:20px;">
+									<div class="col-md-5">
+										<h4>Quantity: </h4>
+										<label>({{$details->remaining}} in stock)</label>
 									</div>
-								</form>
-								<div class="row" style="margin-top:80px;">
-									<div class="col-md-6" >
-										<p>Sell information : {{$seller->name}} {{$seller->surname}}</p>
-										<p>100% positive feedback</p>
-									</div>
-									<div class="col-md-6" >
-										<h3 style="color:#3399FF;">&#9733; &#9733; &#9733; &#9733; &#9734;</h3>
-									</div>
+									<div class="col-md-7" >
+										<input name="quantity_input" type="text" data-validation="number" class="form-control" style="width:100%; float:right;"></input>
+										<input name="idProduct" type="hidden" value={{$product->idProduct}}/>
+									</div>  
 								</div>
 								<div class="row" style="margin-top:20px;">
 									<div class="col-md-12">
-										<a href="QA.html" class="btn btn-default"style="width:100%">Ask a question </a>
+										<button type="submit" class="btn btn-info"style="width:100%">Buy now </button>
 									</div>
+								</div>
+							</form>
+							<div class="row" style="margin-top:80px;">
+								<div class="col-md-6" >
+									<p>Sell information : {{$seller->name}} {{$seller->surname}}</p>
+									<p>100% positive feedback</p>
+								</div>
+								<div class="col-md-6" >
+									<h3 style="color:#3399FF;">&#9733; &#9733; &#9733; &#9733; &#9734;</h3>
 								</div>
 							</div>
-						</div>
-						<div class="col-md-12 column">
-							<div class="well-shadow">
-								<legend>Description</legend>
-								<div class="row" style="margin-top:20px;">
-									<div class="col-md-3 column">
-										<p>Name :</p>
-									</div>
-									<div class="col-md-9 column">
-										<p>{{$product->product_name}}</p>
-									</div>
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-12">
+									<a href="QA.html" class="btn btn-default"style="width:100%">Ask a question </a>
 								</div>
-
-								<div class="row" style="margin-top:20px;">
-									<div class="col-md-3 column">
-										<p>Product Information :</p>
-									</div>
-									<div class="col-md-9 column">
-										<p style="word-wrap: break-word;">{{$product->brand}}</p>
-										<p style="word-wrap: break-word;">{{$product->model}}</p>
-										<p style="word-wrap: break-word;">{{$product->property}}</p>
-										<p style="word-wrap: break-word;">{{$product->condition}}</p>
-										<p style="word-wrap: break-word;">{{$product->defect}}</p>
-									</div>
-								</div>
-
-								<div class="row" style="margin-top:20px;">
-									<div class="col-md-3 column">
-										<p>Technical specification :</p>
-									</div>
-									<div class="col-md-9 column">
-										<p style="word-wrap: break-word;">{{$product->dimension}}</p>
-									</div>
-								</div>
-
-								<div class="row" style="margin-top:20px;">
-									<div class="col-md-3 column">
-										<p>Shipping and payments :</p>
-									</div>
-									<div class="col-md-9 column">
-										<p style="word-wrap: break-word;">{{$product->payment_method}}</p>
-										<p style="word-wrap: break-word;">{{$product->packaging_method}}</p>
-										<p style="word-wrap: break-word;">{{$product->packaging_charge}}</p>
-										<p style="word-wrap: break-word;">{{$product->shipping_method}}</p>
-										<p style="word-wrap: break-word;">{{$product->shipping_fee}}</p>
-									</div>
-								</div>
-
-								<div class="row" style="margin-top:20px;">
-									<div class="col-md-3 column">
-										<p>Return policy :</p>
-									</div>
-									<div class="col-md-9 column">
-										<p style="word-wrap: break-word;">{{$product->refund_policy}}</p>
-										<p style="word-wrap: break-word;">{{$product->refund_charge}}</p>
-									</div>
-								</div>
-
-
 							</div>
 						</div>
 					</div>
-					<div class="col-md-2 column">
+					<div class="col-md-12 column">
+						<div class="well-shadow">
+							<legend>Description</legend>
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-3 column">
+									<p>Name :</p>
+								</div>
+								<div class="col-md-9 column">
+									<p>{{$product->product_name}}</p>
+								</div>
+							</div>
+
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-3 column">
+									<p>Product Information :</p>
+								</div>
+								<div class="col-md-9 column">
+									<p style="word-wrap: break-word;">{{$product->brand}}</p>
+									<p style="word-wrap: break-word;">{{$product->model}}</p>
+									<p style="word-wrap: break-word;">{{$product->property}}</p>
+									<p style="word-wrap: break-word;">{{$product->condition}}</p>
+									<p style="word-wrap: break-word;">{{$product->defect}}</p>
+								</div>
+							</div>
+
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-3 column">
+									<p>Technical specification :</p>
+								</div>
+								<div class="col-md-9 column">
+									<p style="word-wrap: break-word;">{{$product->dimension}}</p>
+								</div>
+							</div>
+
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-3 column">
+									<p>Shipping and payments :</p>
+								</div>
+								<div class="col-md-9 column">
+									<p style="word-wrap: break-word;">{{$product->payment_method}}</p>
+									<p style="word-wrap: break-word;">{{$product->packaging_method}}</p>
+									<p style="word-wrap: break-word;">{{$product->packaging_charge}}</p>
+									<p style="word-wrap: break-word;">{{$product->shipping_method}}</p>
+									<p style="word-wrap: break-word;">{{$product->shipping_fee}}</p>
+								</div>
+							</div>
+
+							<div class="row" style="margin-top:20px;">
+								<div class="col-md-3 column">
+									<p>Return policy :</p>
+								</div>
+								<div class="col-md-9 column">
+									<p style="word-wrap: break-word;">{{$product->refund_policy}}</p>
+									<p style="word-wrap: break-word;">{{$product->refund_charge}}</p>
+								</div>
+							</div>
+
+
+						</div>
 					</div>
 				</div>
-
+				<div class="col-md-2 column">
+				</div>
 			</div>
+
 		</div>
 	</div>
-	<!-- direct -->
-	@stop
+</div>
+<!-- direct -->
+@stop
 
-	@section('footer')
-	<nav class="navbar navbar-default navbar-bottom2" role="navigation">
-		@stop
+@section('footer')
+<nav class="navbar navbar-default navbar-bottom2" role="navigation">
+	@stop
