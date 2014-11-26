ebd05f359412c213ab01bc895943ab6b69337983
Pae <pradinaan@gmail.com>
2014-11-26 21:33:51 +0700

    Pae

--- app/views/perdtye/directtype.blade.php
+++ app/views/perdtye/directtype.blade.php
@@ -28,19 +28,19 @@
 											<div class="item active">
 												<img src="img/i6.png" width="100%"/>
 											</div>
-											
+
 											<div class="item">
 												<img src="img/i6.png" width="100%"/>
 											</div>
 
 										</div>
 
-										<!-- Controls -->
+										<!-- Controls --><!-- 
 										<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
 											<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 											<span class="sr-only">Previous</span>
-										</a>
-										<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+										</a> -->
+										<a class="carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
 											<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 											<span class="sr-only">Next</span>
 										</a>
