d7ab84e5df00d2504c88f85ef85e551131484c35
Pae <pradinaan@gmail.com>
2014-12-06 22:14:16 +0700

    direct&auction

--- app/views/perdtye/auctionsell.blade.php
+++ app/views/perdtye/auctionsell.blade.php
@@ -8,232 +8,245 @@
 			<div class="row clearfix">
 				<div class="col-md-2 column">
 				</div>
+				
 				<div class="col-md-8 column">
-					<!-- <form action="auctiontype.html" method="get"> -->
-					{{ Form::open() }}
-						<legend>Sell Registration form</legend>
+					<div class="well-shadow">
+
+						<!-- <form action="auctiontype.html" method="get"> -->
+						{{ Form::open() }}
+						<legend>Auction Sell Registration form</legend>
+
 						<div class="row clearfix">
-							<div class="col-md-4 column">
-								<h5>Headline :</h5>
+							<div class="col-md-1 column">
 							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="product_name" data-validation="required" id="inputEmail" class="form-control" placeholder="Item Subject">
-							</div>
-						</div>
 
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Upload image :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="file" data-validation="mime size" data-validation-allowing="jpg, png, gif" 
-								data-validation-max-size="2M" id="exampleInputFile" name="pic[]" multiple=""/>
-							</div>
-						</div>
+							<div class="col-md-11 column">
 
-						
-
-						<!-- Begintime -->
-						<div class="row clearfix"  style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Begining Time :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<div class="row clearfix">
-									<div class="col-md-6 column">
-										<div class="input-group clockpicker" data-placement="left" data-align="top" data-autoclose="true">
-											<input name="start_time" type="text" class="form-control" placeholder="HH:MM" name="begintime" data-validation="time">
-											<span class="input-group-addon">
-												<span class="glyphicon glyphicon-time"></span>
-											</span>
-										</div>
+								<div class="row clearfix" style="margin-top:15px;">
+									<div class="col-md-3 column">
+										<h5>Headline :</h5>
 									</div>
-									<div class="col-md-6 column">
-										<div class="form-group">
-											<div class="col-lg-10" style="width:100%">
-												<input name="start_date" type="text" class ="form-control datepicker" data-validation="date" data-validation-format="dd/mm/yyyy" id="inputDate" name="begindate" 
-												placeholder="dd/mm/yyyy">
+									<div class="col-md-9 column">
+										<input type="text" name="product_name" data-validation="required" id="inputEmail" class="form-control" placeholder="Item Subject">
+									</div>
+								</div>
+
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Upload image :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="file" data-validation="mime size" data-validation-allowing="jpg, png, gif" 
+										data-validation-max-size="2M" id="exampleInputFile" name="pic[]" multiple=""/>
+									</div>
+								</div>
+
+
+
+								<!-- Begintime -->
+								<div class="row clearfix"  style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Begining Time :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<div class="row clearfix">
+											<div class="col-md-6 column">
+												<div class="input-group clockpicker" data-placement="left" data-align="top" data-autoclose="true">
+													<input name="start_time" type="text" class="form-control" placeholder="HH:MM" name="begintime" data-validation="time">
+													<span class="input-group-addon">
+														<span class="glyphicon glyphicon-time"></span>
+													</span>
+												</div>
+											</div>
+											<div class="col-md-6 column">
+												<div class="form-group">
+													<div class="col-lg-10" style="width:100%">
+														<input name="start_date" type="text" class ="form-control datepicker" data-validation="date" data-validation-format="dd/mm/yyyy" id="inputDate" name="begindate" 
+														placeholder="dd/mm/yyyy">
+													</div>
+												</div>
 											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-						</div>
-						<!-- endtime -->
-						<div class="row clearfix"  style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Ending Time :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<div class="row clearfix">
-									<div class="col-md-6 column">
-										<div class="input-group clockpicker" data-placement="left" data-align="top" data-autoclose="true">
-											<input name='end_time' type="text" class="form-control" placeholder="HH:MM" name="endtime" data-validation="time">
-											<span class="input-group-addon">
-												<span class="glyphicon glyphicon-time"></span>
-											</span>
-										</div>
+								<!-- endtime -->
+								<div class="row clearfix"  style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Ending Time :</h5>
 									</div>
-									<div class="col-md-6 column">
-										<div class="form-group">
-											<div class="col-lg-10" style="width:100%">
-												<input name='end_date' type="text" class ="form-control datepicker" data-validation="date" data-validation-format="dd/mm/yyyy" id="inputDate" name="enddate" 
-												placeholder="dd/mm/yyyy">
+									<div class="col-md-9 column">
+										<div class="row clearfix">
+											<div class="col-md-6 column">
+												<div class="input-group clockpicker" data-placement="left" data-align="top" data-autoclose="true">
+													<input name='end_time' type="text" class="form-control" placeholder="HH:MM" name="endtime" data-validation="time">
+													<span class="input-group-addon">
+														<span class="glyphicon glyphicon-time"></span>
+													</span>
+												</div>
+											</div>
+											<div class="col-md-6 column">
+												<div class="form-group">
+													<div class="col-lg-10" style="width:100%">
+														<input name='end_date' type="text" class ="form-control datepicker" data-validation="date" data-validation-format="dd/mm/yyyy" id="inputDate" name="enddate" 
+														placeholder="dd/mm/yyyy">
+													</div>
+												</div>
 											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-						</div>
 
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Brand :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="brand" data-validation="required" class="form-control" rows="7"  placeholder="Describe the Brand">
-							</div>
-						</div>
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Model :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="model" class="form-control" rows="7"  placeholder="Describe the model">
-							</div>
-						</div>
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Property :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="property" class="form-control" rows="7"  placeholder="Describe the Property">
-							</div>
-						</div>
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Dimension :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="dimension" class="form-control" rows="7"  placeholder="Describe the Dimension">
-							</div>
-						</div>
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Defect :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="defect" class="form-control" rows="7"  placeholder="Describe the Defect">
-							</div>
-						</div>
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Missing Part :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="missing_part" class="form-control" rows="7"  placeholder="Describe the Missing Part">
-							</div>
-						</div>
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Payment Method :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<select class="form-control" name="payment_method">
-									<option>Credit Card</option>
-									<option>Account</option>
-								</select>
-							</div>
-
-						</div>
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Return Policy :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="return_policy" class="form-control" rows="7"  placeholder="Specify your return policy">
-							</div>
-						</div>
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Packaging Method :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="packaging_method" class="form-control" rows="7"  placeholder="Specify your packaging method">
-							</div>
-						</div>
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Packaging Charge :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="packaging_charge" class="form-control" rows="7"  placeholder="Specify your packaging method">
-							</div>
-						</div>
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Shipping Method :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="shipping_method" class="form-control" rows="7"  placeholder="Specify your shipping method">
-							</div>
-						</div>
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Shipping Fee :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="shipping_fee" class="form-control" rows="7"  placeholder="Specify your shipping fee">
-							</div>
-						</div>
-
-						<!-- price -->
-						<div class="row clearfix"  style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Minimum bid :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input name="minimum_bid" type="text" data-validation="number" data-validation-allowing="float" id="inputEmail" class="form-control" placeholder="Minimum bid">
-							</div>
-						</div>
-
-
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Bidding Range :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type='text' name="bidding_range" class="form-control" rows="7" data-validation="required" placeholder="Range of each bid">
-							</div>
-						</div>
-
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-
-							</div>
-							<div class="col-md-8 column">
-								<div class="row clearfix" style="margin-top:20px">
-									<div class="col-md-6 column">
-										<button type="button" style="width:100%;" class="btn btn-default">Reset</button>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Brand :</h5>
 									</div>
-									<div class="col-md-6 column">
-										<button type="submit" style="float:right; width:100%;" class="btn btn-primary">Confirm</button>
+									<div class="col-md-9 column">
+										<input type="text" name="brand" data-validation="required" class="form-control" rows="7"  placeholder="Describe the Brand">
 									</div>
 								</div>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Model :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="text" name="model" class="form-control" rows="7"  placeholder="Describe the model">
+									</div>
+								</div>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Property :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="text" name="property" class="form-control" rows="7"  placeholder="Describe the Property">
+									</div>
+								</div>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Dimension :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="text" name="dimension" class="form-control" rows="7"  placeholder="Describe the Dimension">
+									</div>
+								</div>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Defect :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="text" name="defect" class="form-control" rows="7"  placeholder="Describe the Defect">
+									</div>
+								</div>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Missing Part :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="text" name="missing_part" class="form-control" rows="7"  placeholder="Describe the Missing Part">
+									</div>
+								</div>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Payment Method :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<select class="form-control" name="payment_method">
+											<option>Credit Card</option>
+											<option>Account</option>
+										</select>
+									</div>
+
+								</div>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Return Policy :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="text" name="return_policy" class="form-control" rows="7"  placeholder="Specify your return policy">
+									</div>
+								</div>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Packaging Method :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="text" name="packaging_method" class="form-control" rows="7"  placeholder="Specify your packaging method">
+									</div>
+								</div>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Packaging Charge :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="text" name="packaging_charge" class="form-control" rows="7"  placeholder="Specify your packaging method">
+									</div>
+								</div>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Shipping Method :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="text" name="shipping_method" class="form-control" rows="7"  placeholder="Specify your shipping method">
+									</div>
+								</div>
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Shipping Fee :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="text" name="shipping_fee" class="form-control" rows="7"  placeholder="Specify your shipping fee">
+									</div>
+								</div>
+
+								<!-- price -->
+								<div class="row clearfix"  style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Minimum bid :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input name="minimum_bid" type="text" data-validation="number" data-validation-allowing="float" id="inputEmail" class="form-control" placeholder="Minimum bid">
+									</div>
+								</div>
+
+
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Bidding Range :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type='text' name="bidding_range" class="form-control" rows="7" data-validation="required" placeholder="Range of each bid">
+									</div>
+								</div>
+
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-4 column">
+
+									</div>
+									<div class="col-md-8 column">
+										<div class="row clearfix" style="margin-top:20px">
+											<div class="col-md-6 column">
+												<button type="button" style="width:100%;" class="btn btn-default">Reset</button>
+											</div>
+											<div class="col-md-6 column">
+												<button type="submit" style="float:right; width:100%;" class="btn btn-primary">Confirm</button>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!-- </form> -->
+								{{ Form::close() }}
 							</div>
 						</div>
-					<!-- </form> -->
-					{{ Form::close() }}
-				</div>
-				<div class="col-md-2 column">
+							</div>
+						</div>
+						<div class="col-md-2 column">
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</div>
-<!-- auction -->
-@stop
+		<!-- auction -->
+		@stop
 
-@section('footer')
-	<nav class="navbar navbar-default navbar-bottom2" role="navigation">
-@stop
+		@section('footer')
+		<nav class="navbar navbar-default navbar-bottom2" role="navigation">
+			@stop
--- app/views/perdtye/directsell.blade.php
+++ app/views/perdtye/directsell.blade.php
@@ -9,28 +9,36 @@
 				<div class="col-md-2 column">
 				</div>
 				<div class="col-md-8 column">
-					<!-- <form action="directtype.html" method="get"> -->
-					{{ Form::open(array('files'=>true)) }}
-						<legend>Sell Registration form</legend>
-						<div class="row clearfix">
-							<div class="col-md-4 column">
-								<h5>Headline :</h5>
-							</div>
-							<div class="col-md-8 column">
-								<input type="text" name="product_name" data-validation="required" id="inputEmail" class="form-control" placeholder="Item Subject">
-							</div>
-						</div>
+					<div class="well-shadow">
+						<!-- <form action="directtype.html" method="get"> -->
+						{{ Form::open(array('files'=>true)) }}
+						<legend>Direct Sell Registration form</legend>
 
-						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
-								<h5>Upload image :</h5>
+						<div class="row clearfix" >
+							<div class="col-md-1 column">
 							</div>
-							<div class="col-md-8 column">
-								<input type="file" data-validation="mime size" data-validation-allowing="jpg, png, gif" 
-								data-validation-max-size="2M" id="exampleInputFile" name="pic[]" multiple="true"/>
-								<!-- {{Form::file('pic[]', array('multiple'=>true));}} -->
-							</div>
-						</div>
+
+							<div class="col-md-11 column">
+
+								<div class="row clearfix" style="margin-top:15px;">
+									<div class="col-md-3 column">
+										<h5>Headline :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="text" name="product_name" data-validation="required" id="inputEmail" class="form-control" placeholder="Item Subject">
+									</div>
+								</div>
+
+								<div class="row clearfix" style="margin-top:10px">
+									<div class="col-md-3 column">
+										<h5>Upload image :</h5>
+									</div>
+									<div class="col-md-9 column">
+										<input type="file" data-validation="mime size" data-validation-allowing="jpg, png, gif" 
+										data-validation-max-size="2M" id="exampleInputFile" name="pic[]" multiple="true"/>
+										<!-- {{Form::file('pic[]', array('multiple'=>true));}} -->
+									</div>
+								</div>
 
 						<!-- <div class="row clearfix" style="margin-top:10px">
 							<div class="col-md-4 column">
@@ -41,58 +49,58 @@
 							</div>
 						</div> -->
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Brand :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input type="text" name="brand" data-validation="required" class="form-control" rows="7"  placeholder="Describe the Brand">
 							</div>
 						</div>
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Model :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input type="text" name="model" class="form-control" rows="7"  placeholder="Describe the model">
 							</div>
 						</div>
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Property :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input type="text" name="property" class="form-control" rows="7"  placeholder="Describe the Property">
 							</div>
 						</div>
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Dimension :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input type="text" name="dimension" class="form-control" rows="7"  placeholder="Describe the Dimension">
 							</div>
 						</div>
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Defect :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input type="text" name="defect" class="form-control" rows="7"  placeholder="Describe the Defect">
 							</div>
 						</div>
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Missing Part :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input type="text" name="missing_part" class="form-control" rows="7"  placeholder="Describe the Missing Part">
 							</div>
 						</div>
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Payment Method :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<select class="form-control" name="payment_method">
 									<option>Credit Card</option>
 									<option>Account</option>
@@ -101,42 +109,42 @@
 
 						</div>
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Return Policy :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input type="text" name="return_policy" class="form-control" rows="7"  placeholder="Specify your return policy">
 							</div>
 						</div>
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Packaging Method :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input type="text" name="packaging_method" class="form-control" rows="7"  placeholder="Specify your packaging method">
 							</div>
 						</div>
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Packaging Charge :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input type="text" name="packaging_charge" class="form-control" rows="7"  placeholder="Specify your packaging method">
 							</div>
 						</div>
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Shipping Method :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input type="text" name="shipping_method" class="form-control" rows="7"  placeholder="Specify your shipping method">
 							</div>
 						</div>
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Shipping Fee :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input type="text" name="shipping_fee" class="form-control" rows="7"  placeholder="Specify your shipping fee">
 							</div>
 						</div>
@@ -144,19 +152,19 @@
 
 
 						<div class="row clearfix"  style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Price :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input name="price" type="text" data-validation="number" data-validation-allowing="float" id="inputEmail" class="form-control" placeholder="Price for 1 Item">
 							</div>
 						</div>
 
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 								<h5>Quantity :</h5>
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<input name="quantity" type="text" id="inputEmail" data-validation="number" class="form-control" placeholder="Total Quantity">
 							</div>
 						</div>
@@ -164,10 +172,10 @@
 						
 
 						<div class="row clearfix" style="margin-top:10px">
-							<div class="col-md-4 column">
+							<div class="col-md-3 column">
 
 							</div>
-							<div class="col-md-8 column">
+							<div class="col-md-9 column">
 								<div class="row clearfix" style="margin-top:20px">
 									<div class="col-md-6 column">
 										<button type="button" style="width:100%;" class="btn btn-default">Reset</button>
@@ -178,20 +186,23 @@
 								</div>
 							</div>
 						</div>
-					<!-- </form> -->
-					{{ Form::close() }}
-
+						<!-- </form> -->
+						{{ Form::close() }}
+					</div>
+					<div class="col-md-1 column">
+					</div>
 				</div>
 				<div class="col-md-2 column">
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
 
 </div>
 <!-- direct -->
 @stop
 
 @section('footer')
-	<nav class="navbar navbar-default navbar-bottom2" role="navigation">
-@stop
+<nav class="navbar navbar-default navbar-bottom2" role="navigation">
+	@stop
