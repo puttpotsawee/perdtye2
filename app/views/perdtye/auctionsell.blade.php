dca82594ba764e5655191a1b3b3e6fc52c59f973
puttpotsawee <junrai82@gmail.com>
2014-12-07 03:28:33 +0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/views/perdtye/auctionsell.blade.php

--- app/views/perdtye/auctionsell.blade.php
+++ app/views/perdtye/auctionsell.blade.php
@@ -258,29 +258,25 @@
 		<nav class="navbar navbar-default navbar-bottom2" role="navigation">
 			@stop
 
-<<<<<<< HEAD
-		@secion('lastScript')
-		<script>
 
-		</script>
-		@stop
-=======
+			@secion('lastScript')
 
-		<script>
-function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
+			<script>
+				function readURL(input) {
+					if (input.files && input.files[0]) {
+						var reader = new FileReader();
 
-            reader.onload = function (e) {
-                $('#blah')
-                    .attr('src', e.target.result)
-                    .width(300)            };
+						reader.onload = function (e) {
+							$('#blah')
+							.attr('src', e.target.result)
+							.width(300)            };
 
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-</script>
-<link class="jsbin" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
-<script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js"></script>
->>>>>>> origin/master
+							reader.readAsDataURL(input.files[0]);
+						}
+					}
+				</script>
+				<link class="jsbin" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
+				<script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+				<script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js"></script>
+
+				@stop
