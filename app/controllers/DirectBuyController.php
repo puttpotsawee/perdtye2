31251d2697a19dce130c6be79c1b48c6ecc6352a
xamieng <xami.eng@gmail.com>
2014-12-08 00:30:49 +0700

    kk

--- app/controllers/DirectBuyController.php
+++ app/controllers/DirectBuyController.php
@@ -12,17 +12,17 @@ class DirectBuyController extends BaseController {
 			return Redirect::back()->with('message','Error you are blacklisted');
 		}
 
-		$quantity = Input::get('quantity_input');
-		$idProduct = Input::get('idProduct');
+		$quantity = 1;
+		$idProduct = 1;
 		$product = Product::find($idProduct);
 		$details = Product_direct::find($idProduct);
 		$seller = Member::find($product->idseller);
-		$address = Address::find($Auth::user()->idmember);
+		$address = Address::where('idmember','=',Auth::user()->idmember)->get()->first();
 
 		if($details->remaining >= $quantity) {
 			return View::make('perdtye/sellconfirm')
 			->with(array('user' => $user, 'product' => $product, 'details' => $details, 'seller' => $seller, 'quantity' => $quantity, 'address' => $address));
-			//return Redirect::back()->with('message','Purchase successful');
+			return Redirect::back()->with('message','Purchase successful');
 		}
 		else {
 			return Redirect::back()->with('message','Error not enough remaining products in stock');
